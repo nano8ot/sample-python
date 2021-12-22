7bbd0e94228ae232875d455f8bc840c195a23196
jacek <jacek.k@me.com>
2021-12-22 19:57:04 +0100

    change message

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        msg = 'Hello! you requested %s' % (self.path)
+        msg = 'Hello me and you! you requested %s' % (self.path)
         self.wfile.write(msg.encode())
 
 
