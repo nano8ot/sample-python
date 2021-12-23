8b129520c5ac0a47e5e3e8813c5aa43cbfac3e3d
jacek <jacek.k@me.com>
2021-12-23 19:26:02 +0100

    smal edit

--- server.py
+++ server.py
@@ -7,12 +7,12 @@ from http import HTTPStatus
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(HTTPStatus.OK)
+        # self.send_response(HTTPStatus.OK)
         # self.end_headers()
         self.path = 'index.html'
         return http.server.SimpleHTTPRequestHandler.do_GET(self)
-        #msg = 'Hello me and you! you requested %s' % (self.path)
-        #self.wfile.write(msg.encode())
+        # msg = 'Hello me and you! you requested %s' % (self.path)
+        # self.wfile.write(msg.encode())
 
 
 port = int(os.getenv('PORT', 80))
